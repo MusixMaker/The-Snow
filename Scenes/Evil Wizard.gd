ef9615f731dc20fcdc80aa29849300d6475cf13f
MusixMaker <82478364+MusixMaker@users.noreply.github.com>
2022-03-07 14:58:29 +1300

    Started on enemy animation and player movement

--- Scenes/Evil Wizard.gd
+++ Scenes/Evil Wizard.gd
@@ -1,5 +1,18 @@
 extends KinematicBody2D
 
+onready var sprite = $AnimatedSprite
 
 func _ready():
-	$AnimatedSprite.play("Attack")
+	pass
+		
+func _physics_process(_delta):
+	
+	if Input.is_action_pressed("attack"):
+		$AnimationPlayer.play("Attack")
+	elif Input.is_action_pressed("ui_left"):
+		$AnimationPlayer.play("Move")
+	elif Input.is_action_pressed("ui_right"):
+		$AnimationPlayer.play("Move")
+	else:
+		$AnimationPlayer.play("Idle")
+		
