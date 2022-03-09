b063762cbeaa18ae6317320583636080c32791a3
MusixMaker <82478364+MusixMaker@users.noreply.github.com>
2022-03-10 10:59:09 +1300

    More player code

---
+++ Codes/Player.gd
@@ -0,0 +1,71 @@
+extends KinematicBody2D
+
+export (int) var speed = 120
+export (int) var jump_speed = -50
+export (int) var gravity = 500
+export (int) var slide_speed = 400
+
+var vel = Vector2.ZERO
+
+export (float) var friction = 10
+export (float) var acceleration = 25
+
+enum{
+	IDLE,
+	RUN,
+	JUMP,
+	STARTJUMP,
+	FALL,
+	ATTACK_1,
+	ATTACK_2,
+	HURT,
+	DIE
+}
+
+#Basic Stats
+onready var state = IDLE
+
+onready var ap = $AnimationPlayer
+
+func _ready():
+	ap.play("Idle")
+	pass # Replace with function body.
+
+func update_animation(anim):
+	pass
+
+func handle_state(state):
+	print(state)
+	match(state):
+		STARTJUMP:
+			vel.y = jump_speed
+	pass
+
+func get_input():
+	var dir = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	if dir != 0:
+		vel.x = move_toward(vel.x, dir*speed, acceleration)
+	else:
+		vel.x = move_toward(vel.x, 0, friction)
+
+func _process(delta):
+	get_input()
+	print(is_on_floor())
+	if vel == Vector2.ZERO:
+		state = IDLE
+	if Input.get_action_strength("ui_up") and is_on_floor():
+		state = STARTJUMP
+	if vel.x != 0:
+		state = RUN
+	
+	if not is_on_floor():
+		if vel.y < 0:
+			JUMP
+		if vel.y > 0:
+			state = FALL
+			
+	handle_state(state)
+	update_animation(state)
+	
+	vel.y += gravity*delta
+	vel = move_and_slide(vel, Vector2.UP)
