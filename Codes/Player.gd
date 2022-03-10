ed48684d00414b441c1164e0534799fc9b7166ed
MusixMaker <82478364+MusixMaker@users.noreply.github.com>
2022-03-11 12:23:10 +1300

    Animations now work, movement also works, now for attack

--- Codes/Player.gd
+++ Codes/Player.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 
-export (int) var speed = 120
-export (int) var jump_speed = -50
+export (int) var speed = 200
+export (int) var jump_speed = -250
 export (int) var gravity = 500
 export (int) var slide_speed = 400
 
@@ -32,7 +32,27 @@ func _ready():
 	pass # Replace with function body.
 
 func update_animation(anim):
-	pass
+	if vel.x < 0:
+		$AnimatedSprite.flip_h = true
+	if vel.x > 0:
+		$AnimatedSprite.flip_h = false
+	match(anim):
+		FALL:
+			ap.play("Fall")
+		IDLE:
+			ap.play("Idle")
+		JUMP:
+			ap.play("Jump")
+		RUN:
+			ap.play("Run")
+		ATTACK_1:
+			ap.play("Attack 1")
+		ATTACK_2:
+			ap.play("Attack 2")
+		HURT:
+			ap.play("Hurt")
+		DIE:
+			ap.play("Death")
 
 func handle_state(state):
 	print(state)
@@ -55,12 +75,12 @@ func _process(delta):
 		state = IDLE
 	if Input.get_action_strength("ui_up") and is_on_floor():
 		state = STARTJUMP
-	if vel.x != 0:
+	elif vel.x != 0:
 		state = RUN
 	
 	if not is_on_floor():
 		if vel.y < 0:
-			JUMP
+			state = JUMP
 		if vel.y > 0:
 			state = FALL
 			
