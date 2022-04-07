efc1a0234bc86f7cc2e5e2291b9a527579178fb3
MusixMaker <82478364+MusixMaker@users.noreply.github.com>
2022-04-07 12:24:55 +1200

    More enemy and player work

--- Codes/Fireball.gd
+++ Codes/Fireball.gd
@@ -1,1 +1,12 @@
-extends Hitbox
+extends Area2D
+
+var direction: Vector2 = Vector2.ZERO
+var fireball_speed: int = 0
+
+func launch(initial_position: Vector2, dir: Vector2, speed: int) -> void:
+	position = initial_position
+	direction = dir
+	fireball_speed = speed
+	
+func _physics_process(delta:float) -> void:
+	position += direction * fireball_speed * delta
--- Codes/Player.gd
+++ Codes/Player.gd
@@ -92,10 +92,8 @@ func _process(delta):
 	#handle_state(state)
 	if vel.x < 0:
 		$AnimatedSprite.flip_h = true
-	#	$AnimatedSprite/HitArea/HitAreaRight.position.x *= 1
 	if vel.x > 0:
 		$AnimatedSprite.flip_h = false
-	#	$AnimatedSprite/HitArea/HitAreaRight.position.x *= -1
 	
 	vel.y += gravity*delta
 	#if is_attacking == true:
