dd18d93e214d4be3493e04c14d33f0c5e38deedd
MusixMaker <82478364+MusixMaker@users.noreply.github.com>
2022-06-07 13:24:52 +1200

    Why enemy not hurt, update some animations

--- Codes/Fireworm.gd
+++ Codes/Fireworm.gd
@@ -68,5 +68,5 @@ func deal_damage():
 	else:
 		state_machine.travel("Hurt")
 		hit = true
-		yield(get_tree().create_timer(0.3), "timeout")
+		yield(get_tree().create_timer(0.4), "timeout")
 		hit = false
