61f2f50d28b50357332eb56cb76bd0fc7917f9c0
MusixMaker <82478364+MusixMaker@users.noreply.github.com>
2022-06-20 09:45:23 +1200

    Some title work

---
+++ Scenes/Title Screen.gd
@@ -0,0 +1,23 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$VBoxContainer/Start.grab_focus()
+
+
+func _on_Start_pressed():
+	get_tree().change_scene("res://Scenes/World.tscn")
+
+
+func _on_Options_pressed():
+	pass # Replace with function body.
+
+
+func _on_Exit_pressed():
+	get_tree().quit()
