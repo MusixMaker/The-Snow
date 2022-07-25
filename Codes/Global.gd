cd52cfd9975b625c67fb7cdca0a19c58858ec21a
MusixMaker <82478364+MusixMaker@users.noreply.github.com>
2022-07-25 12:25:29 +1200

    Some keybinds and minimal pause

--- Codes/Global.gd
+++ Codes/Global.gd
@@ -1,7 +1,16 @@
 extends Node
 
+
+onready var settingsmenu = load("res://Scenes/PauseCanvas.tscn")
 var filepath = "res://keybinds.ini"
 var configfile
+
+var keybinds = {}
+
+func _input(event):
+	if Input.is_key_pressed(KEY_ESCAPE):
+		add_child(settingsmenu.instance())
+		get_tree().paused = true
 
 func _ready():
 	configfile = ConfigFile.new()
@@ -9,6 +18,22 @@ func _ready():
 		for key in configfile.get_section_keys("keybinds"):
 			var key_value = configfile.get_value("keybinds", key)
 			print(key, " : ", OS.get_scancode_string(key_value))
+			
+			keybinds[key] = key_value
 	else:
 		print("CONFIG FILE NOT FOUND")
 		get_tree().change_scene("res://Scenes/File Not Found.tscn")
+		
+	set_game_binds()
+
+func set_game_binds():
+	for key in keybinds.keys():
+		var value = keybinds[key]
+		
+		var actionlist = InputMap.get_action_list(key)
+		if !actionlist.empty():
+			InputMap.action_erase_event(key, actionlist[0])
+			
+		var new_key = InputEventKey.new()
+		new_key.set_scancode(value)
+		InputMap.action_add_event(key, new_key)
--- Codes/Pause.gd
+++ Codes/Pause.gd
@@ -5,18 +5,18 @@ export var muted = false
 export onready var music = $VBoxContainer/HSlider
 export onready var mutey = $"Background/VBoxContainer/MuteColour/Sound"
 export onready var current_noise
-var is_paused = false setget set_is_paused
+#var is_paused = false setget set_is_paused
 
 
-func _unhandled_input(event):
-	if event.is_action_pressed("Pause"):
-		self.is_paused = !is_paused
-		
+#func _unhandled_input(event):
+#	if event.is_action_pressed("Pause"):
+#		self.is_paused = !is_paused
+#		
 
-func set_is_paused(value):
-	is_paused = value
-	get_tree().paused = is_paused
-	visible = is_paused
+#func set_is_paused(value):
+#	is_paused = value
+#	get_tree().paused = is_paused
+#	visible = is_paused
 
 func _physics_process(delta):
 	if music.value < 10:
